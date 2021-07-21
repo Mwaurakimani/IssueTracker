a9a0d7ad0004c230ea45809e8f12a06a4f303893
MwauraKimani <kimmwaus@gmail.com>
2021-07-21 09:12:42 +0300

    Client guard

--- routes/web.php
+++ routes/web.php
@@ -20,12 +20,13 @@ Route::get('/', function () {
 
 
 Route::get('/Dashboard', function () {
-    return view('App.Back.dashboard');
-})->middleware(['auth'])->name('Dashboard');
+    $title = Auth::user()->title;
 
-
-Route::get('/Dashboard', function () {
-    return view('App.Back.dashboard');
+    if($title == 'Admin'){
+        return view('App.Back.dashboard');
+    }else{
+        return redirect('/');
+    }
 })->middleware(['auth'])->name('Dashboard');
 
 Route::get('/{id}/MyIssues', [IssueController::class,'listMyIssues'])->middleware(['auth'])->name('MyIssues');
