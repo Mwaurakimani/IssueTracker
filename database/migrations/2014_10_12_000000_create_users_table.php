3a1766930e2f3aea12d44f5a780f3c8be1cb2ce0
MwauraKimani <kimmwaus@gmail.com>
2021-08-04 08:35:31 +0300

    change to title users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('email');
-            $table->string("title");
+            $table->string("title")->default('Client');
             $table->string("phone");
             $table->unsignedBigInteger("group_id");
             $table->unsignedBigInteger("role_id");
